6a811672b164b9c26e158ef0478fcde277d07a01
AdrianDias444 <adriandias0709@gmail.com>
2025-10-15 21:03:37 +0100

    ex24 done

---
+++ ex24/main.c
@@ -0,0 +1,10 @@
+#include "includes/header.h"
+
+int main(void)
+{
+    ft_putstr("<<<<<<<<<< FT_PUTCHAR >>>>>>>>>>\n");
+    ft_putchar('a');
+    ft_putchar('\n');
+    ft_putstr("<<<<<<<<<< FT_PUTSTR >>>>>>>>>>\n");
+    ft_putstr("ft_putstr is working");
+}
